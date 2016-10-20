461c3b53e18b153e5dbb773220cfb9cf3ee01227
橋本泰平 <taihei@hashimototaihei-no-MacBook-Air.local>
2016-10-21 08:11:07 +0900

    update20161021

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,103 +1,21 @@
 #coding: utf-8
 @page = Page.new
-@page.number = 99
-@page.title = "アスキーのニコ生「デジデジ90」でアダルト（エロ）VR特集が配信されました！！"
-@page.url = "http://protosketch.co/archives/adult_category/adultinfo"
-@page.image = "/images/2-1.jpg"
-@page.body = ""
-@page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
-@page.date = "2016/10/20"
+@page.number = 101
+@page.title = "PlayStation VRでVRアダルトゲームや動画を楽しめるか試してみた【アダルトご注意】"
+@page.url = "http://ascii.jp/limit/group/ida/elem/000/001/250/1250938/"
+@page.image = "/images/00a_ogp.jpg"
+@page.body = "ついに発売したPlayStation VR。パンツが覗けないとお嘆きのあなたに贈る、アダルトコンテンツの楽しむ方法を多数実験したみた。"
+@page.site_name = "ASCII.jp"
+@page.date = "2016/10/21"
 @page.save
 
 @page = Page.new
-@page.number = 98
-@page.title = "世界初のVR専用エロゲ『VRカノジョ』、Steam版はあくまで体験版　セクシャル要素は少なめに：はちま起稿"
-@page.url = "http://blog.esuteru.com/archives/20003735.html"
-@page.image = "/images/noimage_tb.png"
-@page.body = "世界初のVR専用エロゲ『VRカノジョ』、Steam版はあくまで体験版　セクシャル要素は少なめにの記事詳細。国内最大のエンタメまとめメディアサイト。ゲーム情報、アニメや漫画、時事ネタなど、たくさんのネタを面白おかしく紹介します。"
-@page.site_name = "はちま起稿"
-@page.date = "2016/10/20"
-@page.save
-
-@page = Page.new
-@page.number = 97
-@page.title = "エロゲ製作者「VRの発展にエロは必須要素、アダルトには認知度をあげる役割がある」：はちま起稿"
-@page.url = "https://blog.esuteru.com/archives/20003887.html"
-@page.image = "/images/54643c463ff481d56e703c39a76ace72_tb.jpg"
-@page.body = "エロゲ製作者「VRの発展にエロは必須要素、アダルトには認知度をあげる役割がある」の記事詳細。国内最大のエンタメまとめメディアサイト。ゲーム情報、アニメや漫画、時事ネタなど、たくさんのネタを面白おかしく紹介します。"
-@page.site_name = "はちま起稿"
-@page.date = "2016/10/20"
-@page.save
-
-@page = Page.new
-@page.number = 96
-@page.title = "【画像】VRセックス、えっち過ぎるンゴォォォオオオオｗｗｗｗｗ :【2ch】ニュー速クオリティ"
-@page.url = "http://news4vip.livedoor.biz/archives/52188617.html"
-@page.image = "/images/89c01796.jpg"
-@page.body = "1: 風吹けば名無し＠＼(^o^)／ 2016/10/12(水) 23:59:39.92 ID:ID:/WzSoCvcp.net やりたいンゴ  ttp://i.kinja-img.com/gawker-media/image/upload/t_original/1405362014611195427.gif   引用元: VRセックス、えっち過ぎるンゴォォォオオオオ！！！！"
-@page.site_name = "【2ch】ニュー速クオリティ"
-@page.date = "2016/10/20"
-@page.save
-
-@page = Page.new
-@page.number = 95
-@page.title = "【朗報】サマーレッスンそっくりのVRエロゲ、Steam進出：はちま起稿"
-@page.url = "http://blog.esuteru.com/archives/20003662.html"
-@page.image = "/images/033b1068d2e564afd985246a12218df1_tb.jpg"
-@page.body = "【朗報】サマーレッスンそっくりのVRエロゲ、Steam進出の記事詳細。国内最大のエンタメまとめメディアサイト。ゲーム情報、アニメや漫画、時事ネタなど、たくさんのネタを面白おかしく紹介します。"
-@page.site_name = "はちま起稿"
-@page.date = "2016/10/20"
-@page.save
-
-@page = Page.new
-@page.number = 94
-@page.title = "VR元年、ついに山が動く......！ 今秋に配信をスタートする「DMM.com」が放つVR動画とは!?【インタビュー前編】（おたぽる） - グノシー"
-@page.url = "https://gunosy.com/articles/RQTbL"
-@page.image = "/images/825c9e5f9e8c573b68a0b2ebdcb9d740_content.jpg"
-@page.body = "
-　もはや使い古された慣用句のようになりつつあるが、Oculus RiftやHTC Vive、Gear VRが国内でも発売され、そして10月13日に「PlayStation VR」（以下、「PSVR」）も発売となり、“VR元年”と称される2016年。
-　先月開催された「東京ゲームショウ2016」（9月15～18日／以下「TGS」）でも、VRモードを搭載したゲームやデバイス、専用機器が数多く展示され"
-@page.site_name = "グノシー 雑談力がつくニュースアプリ"
-@page.date = "2016/10/20"
-@page.save
-
-@page = Page.new
-@page.number = 93
-@page.title = "PS4でVRやって「これをエロに使ったら……」と思った人が見るページ - VR中心、雑記ブログ"
-@page.url = "http://voya.hatenablog.com/entry/2016/10/16/165249"
-@page.image = "/images/20161016153057.jpg"
-@page.body = "このページでは、エロに関心のあるVRのドアを叩いた人の水先案内人として、VRエロについて紹介します。 PSVRどうでした？「VRって『体験しないとわからない』とか言われてるけど、ほんとそうだな」と思いました？あるいは「うーん、いまいちだけど、とりあえずこんな感じのでエロってあるのかな？」と思いました？ エロ、あります。エロVRの世界をご案内いたします。 サマーレッスンやって、パンツ覗きたいと思いませんでした？あるいは、キスしたい、とか。そういうのできます。（パンツ覗くのはアダルトVRゲームならば当たり前にできます。（ただし覗かれたことに対する恥じらいとかを実装したゲームはないかな。）キスのコマ…"
-@page.site_name = "VR中心、雑記ブログ"
-@page.date = "2016/10/20"
-@page.save
-
-@page = Page.new
-@page.number = 92
-@page.title = "ついに日本人はVRで生物としての一線を超えてしまうのか"
-@page.url = "http://anond.hatelabo.jp/20161017184349"
-@page.image = "/images/og-image-1500.gif"
-@page.body = "VRのアダルトコンテンツとオナホを連動させる技術が開発中らしいが、これが実用化されるとマジで日本人絶滅待ったなしだと思う。特に男にとって…"
-@page.site_name = "はてな匿名ダイアリー"
-@page.date = "2016/10/20"
-@page.save
-
-@page = Page.new
-@page.number = 91
-@page.title = "VRで楽しめる今のところのVRエロゲーをまとめる（2016/10/16） - VR中心、雑記ブログ"
-@page.url = "http://voya.hatenablog.com/entry/2016/10/16/185313"
-@page.image = "/images/20161016174518.jpg"
-@page.body = "まず注意。現在あるVRエロゲーは、ほとんどPCと、Oculus RiftあるいはHTC Viveを使って遊ぶものです。スマホVRでのアダルトVRアプリもあるにはあるようですが。（あるいはスマホならば特殊なアプリなどを使ってPC用のVRゲームがプレイ可能かもしれませんが、おすすめはいたしません。） 現状のVRでエロを追求しようとすると、まずもって自分で色々と調べられる根気がなければきついです。とはいえ、入り口がまったくないのも困ったもの。ということで、一応自分が知っている限りで色々まとめますが、文句は勘弁してください。（このブログはコメントできない設定なので、文句を言う場所がないと思いますが。）…"
-@page.site_name = "VR中心、雑記ブログ"
-@page.date = "2016/10/20"
-@page.save
-
-@page = Page.new
-@page.number = 90
-@page.title = "アダルトVRゲーム『VRカノジョ』がSteam Greenlightに進出。その狙いは？　意気込みは？　イリュージョンに聞いてみた"
-@page.url = "http://nlab.itmedia.co.jp/nl/articles/1610/15/news041.html"
-@page.image = "/images/l_ah_kanojo0.jpg"
-@page.body = "「VRカノジョ Trial」がSteam Greenlightに。なぜこのタイミングでSteamへ進出しようと思ったのかを聞いた。"
-@page.site_name = "ねとらぼ"
-@page.date = "2016/10/20"
+@page.number = 100
+@page.title = "「VRの普及にはエロが必要」VR専用アダルトゲーム「VRカノジョ」開発者が語る【アダルトご注意】"
+@page.url = "http://ascii.jp/limit/group/ida/elem/000/001/249/1249687/"
+@page.image = "/images/02_ogp.jpg"
+@page.body = "話題沸騰中のアダルトゲームブランド「イリュージョン」の中の人が語るアダルトコンテンツが切り開くVRの未来。"
+@page.site_name = "ASCII.jp"
+@page.date = "2016/10/21"
 @page.save
 
