08fc0130c7c0b0c466129215f7e7a09462c4a67d
橋本泰平 <taihei@hashimototaihei-no-MacBook-Air.local>
2016-10-22 16:09:37 +0900

    update20161022

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,51 @@
 #coding: utf-8
 @page = Page.new
-@page.number = 101
-@page.title = "PlayStation VRでVRアダルトゲームや動画を楽しめるか試してみた【アダルトご注意】"
-@page.url = "http://ascii.jp/limit/group/ida/elem/000/001/250/1250938/"
-@page.image = "/images/00a_ogp.jpg"
-@page.body = "ついに発売したPlayStation VR。パンツが覗けないとお嘆きのあなたに贈る、アダルトコンテンツの楽しむ方法を多数実験したみた。"
-@page.site_name = "ASCII.jp"
-@page.date = "2016/10/21"
+@page.number = 106
+@page.title = "アダルト（エロ）VR動画で阿部乃みく （あべのみく）に中出ししちゃおう！"
+@page.url = "http://protosketch.co/archives/adult_category/%E3%82%A2%E3%83%80%E3%83%AB%E3%83%88vr%E5%8B%95%E7%94%BB%E9%85%8D%E4%BF%A1%E3%82%B5%E3%82%A4%E3%83%88"
+@page.image = "/images/abe.jpg"
+@page.body = ""
+@page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
+@page.date = "2016/10/22"
 @page.save
 
 @page = Page.new
-@page.number = 100
-@page.title = "「VRの普及にはエロが必要」VR専用アダルトゲーム「VRカノジョ」開発者が語る【アダルトご注意】"
-@page.url = "http://ascii.jp/limit/group/ida/elem/000/001/249/1249687/"
-@page.image = "/images/02_ogp.jpg"
-@page.body = "話題沸騰中のアダルトゲームブランド「イリュージョン」の中の人が語るアダルトコンテンツが切り開くVRの未来。"
-@page.site_name = "ASCII.jp"
-@page.date = "2016/10/21"
+@page.number = 105
+@page.title = "ギャル系AV女優「長谷川夏樹（はせがわなつき）」をアダルト（エロ）VR動画で堪能！！"
+@page.url = "http://protosketch.co/archives/adult_category/adultvr-movie_zissya"
+@page.image = "/images/hasw.jpg"
+@page.body = ""
+@page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
+@page.date = "2016/10/22"
+@page.save
+
+@page = Page.new
+@page.number = 104
+@page.title = "完全引退！みづなれいの最初で最後のアダルト（エロ）VR動画シリーズ配信開始！"
+@page.url = "http://protosketch.co/archives/adult_category/onany"
+@page.image = "/images/gallery_3.jpg"
+@page.body = "今年で完全引退する人気AV女優のみづなれいのアダルト（エロ）VR動画が登場しました。引退前の最初で最後のアダルト（エロ）VR動画です。気になるそのアダルト（エロ）VR動画シリーズ作品を一挙紹介致します。"
+@page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
+@page.date = "2016/10/22"
+@page.save
+
+@page = Page.new
+@page.number = 103
+@page.title = "「Adult festa VR（アダルトフェスタVR）」にてアダルトVR動画配信中！視聴方法など詳細情報お届け！"
+@page.url = "http://protosketch.co/archives/adult/1059"
+@page.image = "/images/festavr.jpg"
+@page.body = ""
+@page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
+@page.date = "2016/10/22"
+@page.save
+
+@page = Page.new
+@page.number = 102
+@page.title = "話題沸騰「プレステVR」対抗エロパワー！「VRカノジョ」凄すぎ！"
+@page.url = "http://mnsatlas.com/?p=12227"
+@page.image = "/images/03-3.png"
+@page.body = "10月13日、SONYは自宅で手軽にバーチャルリアリティを楽しめる機器「プレステVR」を発売した。 価格は49,980円と高額ながらハイクオリティのバーチャル画面が体験できるということで世界中でも話題になっている。 (adsbygoogle"
+@page.site_name = "ATLAS"
+@page.date = "2016/10/22"
 @page.save
 
