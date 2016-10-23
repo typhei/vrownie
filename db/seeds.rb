fe5f120a6c753aef25c6f26d3b7f6ce43de5c18c
橋本泰平 <taihei@hashimototaihei-no-MacBook-Air.local>
2016-10-24 03:03:19 +0900

    update20161024

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,11 @@
 #coding: utf-8
 @page = Page.new
-@page.number = 109
-@page.title = "女医「仁美まどか（ひとみまどか）」先生に襲われちゃうアダルト（エロ）VR動画が登場！"
-@page.url = "http://protosketch.co/archives/adult_category/hbox-vr"
-@page.image = "/images/hitomi.jpg"
-@page.body = "人気AV女優「仁美まどか（ひとみまどか）」が女医となって相談にやってきたあなたを襲っちゃうアダルト（エロ）VR動画が「HBOX VR」にてリリースされました。エロさムンムンの女医に優しく筆おろししてもらえる、体験ができちゃいます。"
-@page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
-@page.date = "2016/10/23"
-@page.save
-
-@page = Page.new
-@page.number = 108
-@page.title = "【VR】「VRの普及にはエロが必要」VR専用アダルトゲーム開発者が語る！！ : VRNEWS速報"
-@page.url = "http://vrnews-sokuhou.com/archives/8294227.html"
-@page.image = "/images/7bc5d158.jpg"
-@page.body = " "
-@page.site_name = "VRNEWS速報"
-@page.date = "2016/10/23"
-@page.save
-
-@page = Page.new
-@page.number = 107
-@page.title = "VRで女子高生と接近！シャドバでおっぱいの小さい女の子たちが！？セレナの名場面がフィギュアに！ほか昨日の注目記事いっき読み！ «  Nxゲーマーズ"
-@page.url = "http://nxgamers.jp/30896"
-@page.image = "/images/3815-468x264.png"
-@page.body = "ニンテンドー3DSやまだ見ぬ新世代機の面白いゲームに出会える！VRで女子高生と接近！シャドバでおっぱいの小さい女の子たちが！？セレナの名場面がフィギュアに！ほか昨日の注目記事いっき読み！"
-@page.site_name = "Nxゲーマーズ"
-@page.date = "2016/10/23"
+@page.number = 110
+@page.title = "判明：世間のVRへの関心は「アダルト」に集まる【倶楽部】"
+@page.url = "http://ascii.jp/elem/000/001/251/1251467/"
+@page.image = "/images/01_ogp.jpg"
+@page.body = "先週の11日から、ASCII倶楽部では「VR特集」を実施中。さまざまな特集記事が公開されています！"
+@page.site_name = "ASCII.jp"
+@page.date = "2016/10/24"
 @page.save
 
