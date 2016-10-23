43281f61646a2c16b7307febfd291235814921a3
橋本泰平 <taihei@hashimototaihei-no-MacBook-Air.local>
2016-10-23 09:10:12 +0900

    update20161023

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,31 @@
 #coding: utf-8
 @page = Page.new
-@page.number = 106
-@page.title = "アダルト（エロ）VR動画で阿部乃みく （あべのみく）に中出ししちゃおう！"
-@page.url = "http://protosketch.co/archives/adult_category/%E3%82%A2%E3%83%80%E3%83%AB%E3%83%88vr%E5%8B%95%E7%94%BB%E9%85%8D%E4%BF%A1%E3%82%B5%E3%82%A4%E3%83%88"
-@page.image = "/images/abe.jpg"
-@page.body = ""
+@page.number = 109
+@page.title = "女医「仁美まどか（ひとみまどか）」先生に襲われちゃうアダルト（エロ）VR動画が登場！"
+@page.url = "http://protosketch.co/archives/adult_category/hbox-vr"
+@page.image = "/images/hitomi.jpg"
+@page.body = "人気AV女優「仁美まどか（ひとみまどか）」が女医となって相談にやってきたあなたを襲っちゃうアダルト（エロ）VR動画が「HBOX VR」にてリリースされました。エロさムンムンの女医に優しく筆おろししてもらえる、体験ができちゃいます。"
 @page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
-@page.date = "2016/10/22"
+@page.date = "2016/10/23"
 @page.save
 
 @page = Page.new
-@page.number = 105
-@page.title = "ギャル系AV女優「長谷川夏樹（はせがわなつき）」をアダルト（エロ）VR動画で堪能！！"
-@page.url = "http://protosketch.co/archives/adult_category/adultvr-movie_zissya"
-@page.image = "/images/hasw.jpg"
-@page.body = ""
-@page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
-@page.date = "2016/10/22"
+@page.number = 108
+@page.title = "【VR】「VRの普及にはエロが必要」VR専用アダルトゲーム開発者が語る！！ : VRNEWS速報"
+@page.url = "http://vrnews-sokuhou.com/archives/8294227.html"
+@page.image = "/images/7bc5d158.jpg"
+@page.body = " "
+@page.site_name = "VRNEWS速報"
+@page.date = "2016/10/23"
 @page.save
 
 @page = Page.new
-@page.number = 104
-@page.title = "完全引退！みづなれいの最初で最後のアダルト（エロ）VR動画シリーズ配信開始！"
-@page.url = "http://protosketch.co/archives/adult_category/onany"
-@page.image = "/images/gallery_3.jpg"
-@page.body = "今年で完全引退する人気AV女優のみづなれいのアダルト（エロ）VR動画が登場しました。引退前の最初で最後のアダルト（エロ）VR動画です。気になるそのアダルト（エロ）VR動画シリーズ作品を一挙紹介致します。"
-@page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
-@page.date = "2016/10/22"
-@page.save
-
-@page = Page.new
-@page.number = 103
-@page.title = "「Adult festa VR（アダルトフェスタVR）」にてアダルトVR動画配信中！視聴方法など詳細情報お届け！"
-@page.url = "http://protosketch.co/archives/adult/1059"
-@page.image = "/images/festavr.jpg"
-@page.body = ""
-@page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
-@page.date = "2016/10/22"
-@page.save
-
-@page = Page.new
-@page.number = 102
-@page.title = "話題沸騰「プレステVR」対抗エロパワー！「VRカノジョ」凄すぎ！"
-@page.url = "http://mnsatlas.com/?p=12227"
-@page.image = "/images/03-3.png"
-@page.body = "10月13日、SONYは自宅で手軽にバーチャルリアリティを楽しめる機器「プレステVR」を発売した。 価格は49,980円と高額ながらハイクオリティのバーチャル画面が体験できるということで世界中でも話題になっている。 (adsbygoogle"
-@page.site_name = "ATLAS"
-@page.date = "2016/10/22"
+@page.number = 107
+@page.title = "VRで女子高生と接近！シャドバでおっぱいの小さい女の子たちが！？セレナの名場面がフィギュアに！ほか昨日の注目記事いっき読み！ «  Nxゲーマーズ"
+@page.url = "http://nxgamers.jp/30896"
+@page.image = "/images/3815-468x264.png"
+@page.body = "ニンテンドー3DSやまだ見ぬ新世代機の面白いゲームに出会える！VRで女子高生と接近！シャドバでおっぱいの小さい女の子たちが！？セレナの名場面がフィギュアに！ほか昨日の注目記事いっき読み！"
+@page.site_name = "Nxゲーマーズ"
+@page.date = "2016/10/23"
 @page.save
 
