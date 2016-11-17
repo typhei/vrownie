03344ffaab8591a9c49b1090691574b3cc30ec73
typhee <taihei.hashimoto@vrownie.com>
2016-11-17 15:41:37 +0000

    update20161117

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,81 +1,51 @@
-#coding:utf-8
-@video = Video.new
-@video.title = "vr_has_never_been_this_real_with_cory_chase_and_avalon_heart"
-@video.url = "http://www.xvideos.com/video24519335/vr_has_never_been_this_real_with_cory_chase_and_avalon_heart"
-@video.image = "/xvideos/4079cf5ae4d35267f3f324a0e948a6a8.11.jpg"
-@video.site_name = "XVIDEOS.COM"
-@video.Date = "20161115"
-@video.save
+#coding: utf-8
+@page = Page.new
+@page.number = 121
+@page.title = "おすすめの巨乳アダルト（エロ）VR動画一挙紹介！！"
+@page.url = "http://protosketch.co/archives/adult/4164"
+@page.image = "/images/kyonyuu.jpg"
+@page.body = ""
+@page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
+@page.date = "2016/11/17"
+@page.save
 
-@video = Video.new
-@video.title = "白い変態汁ぶっかけて!"
-@video.url = "http://www.afesta.tv/vr/face.php?fid=e736e967a4fb2cdd7c72472f72eb369e"
-@video.image = "/adultfesta/afes219-220.jpg"
-@video.site_name = "Adult festa VR"
-@video.Date = "20161115"
-@video.save
+@page = Page.new
+@page.number = 120
+@page.title = "アダルトVRフェスで沢山のバーチャルおっぱいを揉んできたお話"
+@page.url = "http://kai-you.net/article/29885"
+@page.image = "/images/oppai.jpg"
+@page.body = "アダルトコンテンツをメインとしたVR（バーチャルリアリティ）を体験できるイベント「アダルトVRフェスタ01」が6月12日（日）に開催。開場からわずか1時間足らずで入場規制、しまいには人が集まりすぎて開催中止・延期となってしまうほどの大盛況ぶりで幕を閉じた。こ..."
+@page.site_name = "KAI-YOU.net | POP is Here ."
+@page.date = "2016/11/17"
+@page.save
 
-@video = Video.new
-@video.title = "巨乳人妻と不倫セックス！"
-@video.url = "http://www.afesta.tv/vr/face.php?fid=cb91d4b15e29d8610606829ad9d496c9"
-@video.image = "/adultfesta/vdog148.jpg"
-@video.site_name = "Adult festa VR"
-@video.Date = "20161115"
-@video.save
+@page = Page.new
+@page.number = 119
+@page.title = "人気すぎて開催中止の「アダルトVRフェスタ」リベンジは8月！ チケット販売開始"
+@page.url = "http://kai-you.net/article/30413"
+@page.image = "/images/re5acc0f89addf0a60138173851125356f.png"
+@page.body = "アダルトコンテンツをメインとしたVR（バーチャルリアリティ）を体験できるイベント「アダルトVRフェスタ」の第2回が8月に開催決定。入場規制・開催中止を余儀なくされた前回の反省を踏まえ、チケットは事前購入制となる。「アダルトVRフェスタ02」の場所や日程、チ..."
+@page.site_name = "KAI-YOU.net | POP is Here ."
+@page.date = "2016/11/17"
+@page.save
 
-@video = Video.new
-@video.title = "お兄ちゃん大好き！ロリカワ妹とラブラブセックス"
-@video.url = "http://www.afesta.tv/vr/face.php?fid=bd0aab28c3cc521c85a388cf921b23fd"
-@video.image = "/adultfesta/vdog150.jpg"
-@video.site_name = "Adult festa VR"
-@video.Date = "20161115"
-@video.save
+@page = Page.new
+@page.number = 118
+@page.title = "GearVRで始めよう。アダルトＶＲ動画の視聴方法【最新版】"
+@page.url = "http://vr-adultporn.com/?p=266"
+@page.image = "/images/Gear-VR.png"
+@page.body = "Gear VRでアダルトＶＲ動画を見よう！ みなさん、エロティカルなＶＲ動画に興味はおありでしょうか？ 興味が…"
+@page.site_name = "アダルトVR速報"
+@page.date = "2016/11/17"
+@page.save
 
-@video = Video.new
-@video.title = "紳士の夜這い　vol２"
-@video.url = "http://www.afesta.tv/vr/face.php?fid=be41030ab69b7441fadd360dd30f3e26"
-@video.image = "/adultfesta/vdog152.jpg"
-@video.site_name = "Adult festa VR"
-@video.Date = "20161115"
-@video.save
-
-@video = Video.new
-@video.title = "激カワロリ妹の本気オナニー"
-@video.url = "http://www.afesta.tv/vr/face.php?fid=0f3e7b8817fecff35a4a508f0b368282"
-@video.image = "/adultfesta/vdog147.jpg"
-@video.site_name = "Adult festa VR"
-@video.Date = "20161115"
-@video.save
-
-@video = Video.new
-@video.title = "紳士の夜這い　vol１"
-@video.url = "http://www.afesta.tv/vr/face.php?fid=4e4b328639d9be3334dab78a1527ddcd"
-@video.image = "/adultfesta/vdog149.jpg"
-@video.site_name = "Adult festa VR"
-@video.Date = "20161115"
-@video.save
-
-@video = Video.new
-@video.title = "男のロマン！vol3　ロリ妹着替え盗撮"
-@video.url = "http://www.afesta.tv/vr/face.php?fid=11cf450cdb16d9a8bde40b028f17b127"
-@video.image = "/adultfesta/vdog151.jpg"
-@video.site_name = "Adult festa VR"
-@video.Date = "20161115"
-@video.save
-
-@video = Video.new
-@video.title = "ダメ…夫にバレちゃう。秘密の台所プレイ"
-@video.url = "http://www.afesta.tv/vr/face.php?fid=64607ef946025d12b009fbc0e95b6002"
-@video.image = "/adultfesta/vdog145-146.jpg"
-@video.site_name = "Adult festa VR"
-@video.Date = "20161115"
-@video.save
-
-@video = Video.new
-@video.title = "ロリ妹と巨乳姉にオナホで搾り取られるVR"
-@video.url = "http://www.afesta.tv/vr/face.php?fid=09ae4ca0217175acc0a1c7467a3f7f17"
-@video.image = "/adultfesta/vdog143-144.jpg"
-@video.site_name = "Adult festa VR"
-@video.Date = "20161115"
-@video.save
+@page = Page.new
+@page.number = 117
+@page.title = "PlayStation VRでVRアダルト動画をちゃんと見られる方法があるってホント？【アダルトご注意】"
+@page.url = "http://ascii.jp/limit/group/ida/elem/000/001/267/1267454/"
+@page.image = "/images/00a_ogp.jpg"
+@page.body = "ついに禁断の領域に足を踏み入れることにしました。"
+@page.site_name = "ASCII.jp"
+@page.date = "2016/11/17"
+@page.save
 
