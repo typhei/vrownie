e4e8e2dc8e4079be8e5272d808a06bb616c2e1a1
typhee <taihei.hashimoto@vrownie.com>
2016-11-18 14:02:26 +0000

    update20161118

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,34 @@
 #coding: utf-8
 @page = Page.new
-@page.number = 121
-@page.title = "おすすめの巨乳アダルト（エロ）VR動画一挙紹介！！"
-@page.url = "http://protosketch.co/archives/adult/4164"
-@page.image = "/images/kyonyuu.jpg"
-@page.body = ""
-@page.site_name = "VR通信｜ゲーム、ニュース、レビュー、価格比較はここで！日本最大級のVR(バーチャルリアリティー)情報サイト"
-@page.date = "2016/11/17"
-@page.save
-
-@page = Page.new
-@page.number = 120
-@page.title = "アダルトVRフェスで沢山のバーチャルおっぱいを揉んできたお話"
-@page.url = "http://kai-you.net/article/29885"
-@page.image = "/images/oppai.jpg"
-@page.body = "アダルトコンテンツをメインとしたVR（バーチャルリアリティ）を体験できるイベント「アダルトVRフェスタ01」が6月12日（日）に開催。開場からわずか1時間足らずで入場規制、しまいには人が集まりすぎて開催中止・延期となってしまうほどの大盛況ぶりで幕を閉じた。こ..."
-@page.site_name = "KAI-YOU.net | POP is Here ."
-@page.date = "2016/11/17"
-@page.save
-
-@page = Page.new
-@page.number = 119
-@page.title = "人気すぎて開催中止の「アダルトVRフェスタ」リベンジは8月！ チケット販売開始"
-@page.url = "http://kai-you.net/article/30413"
-@page.image = "/images/re5acc0f89addf0a60138173851125356f.png"
-@page.body = "アダルトコンテンツをメインとしたVR（バーチャルリアリティ）を体験できるイベント「アダルトVRフェスタ」の第2回が8月に開催決定。入場規制・開催中止を余儀なくされた前回の反省を踏まえ、チケットは事前購入制となる。「アダルトVRフェスタ02」の場所や日程、チ..."
-@page.site_name = "KAI-YOU.net | POP is Here ."
-@page.date = "2016/11/17"
-@page.save
-
-@page = Page.new
-@page.number = 118
-@page.title = "GearVRで始めよう。アダルトＶＲ動画の視聴方法【最新版】"
-@page.url = "http://vr-adultporn.com/?p=266"
-@page.image = "/images/Gear-VR.png"
-@page.body = "Gear VRでアダルトＶＲ動画を見よう！ みなさん、エロティカルなＶＲ動画に興味はおありでしょうか？ 興味が…"
+@page.number = 124
+@page.title = "PlaystationVRでアダルト動画を見る方法【ＰＳＶＲ】"
+@page.url = "http://vr-adultporn.com/?p=453"
+@page.image = "/images/playstationVR.jpg"
+@page.body = "Playstation VRで普通のエロ動画(AV)はもちろんエロＶＲ(AVVR)だって見る事が出来ます 最初…"
 @page.site_name = "アダルトVR速報"
-@page.date = "2016/11/17"
+@page.date = "2016/11/18"
 @page.save
 
 @page = Page.new
-@page.number = 117
-@page.title = "PlayStation VRでVRアダルト動画をちゃんと見られる方法があるってホント？【アダルトご注意】"
-@page.url = "http://ascii.jp/limit/group/ida/elem/000/001/267/1267454/"
-@page.image = "/images/00a_ogp.jpg"
-@page.body = "ついに禁断の領域に足を踏み入れることにしました。"
-@page.site_name = "ASCII.jp"
-@page.date = "2016/11/17"
+@page.number = 123
+@page.title = "「上からおっぱいが降ってきた~！」の雄叫びが飛び出す！ フジテレビVR動画がガチ（J-CASTニュース） - グノシー"
+@page.url = "https://gunosy.com/articles/RBW3a"
+@page.image = "/images/021dd7e7c33d985e7a1302e69fe75a05_content.jpg"
+@page.body = "民放の雄と呼ばれたフジテレビはいま、VR事業に積極的に取り組んでいる。2016年11月15日、オリジナルVRコンテンツが視聴できるスマホアプリ「
+11月16日現在、以下の10コンテンツを配信している。ストリーミング形式ではなく、各コンテンツのファイルをダウンロードして視聴する。
+（1）永島優美アナのヴァーチャルお忍びデート
+（1）は、朝の情報番組「めざましテレビ」でメインキャスターを務める永島優美"
+@page.site_name = "グノシー 雑談力がつくニュースアプリ"
+@page.date = "2016/11/18"
+@page.save
+
+@page = Page.new
+@page.number = 122
+@page.title = "「上からおっぱいが降ってきた～！」の雄叫びが飛び出す！　フジテレビVR動画がガチ"
+@page.url = "http://www.j-cast.com/trend/2016/11/17283669.html"
+@page.image = "/images/trend_20161117131703.jpg"
+@page.body = "   民放の雄と呼ばれたフジテレビはいま、VR事業に積極的に取り組んでいる。2016年11月15日、オリジナルVRコンテンツが視聴できるスマホアプリ「FOD VR」の配信を開始した。    11月16日現在、以下の10コンテンツを配信している。ストリーミング形式ではなく、各コンテンツのファイルをダウンロードして視聴する。 （1）永島優美ア"
+@page.site_name = "J-CASTトレンド"
+@page.date = "2016/11/18"
 @page.save
 
