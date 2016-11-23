3301ef63e63271223dfe3493d537567c100ff2e1
typhee <taihei.hashimoto@vrownie.com>
2016-11-23 04:52:06 +0000

    update20161123

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,105 @@
 #coding:utf-8
 @video = Video.new
-@video.title = "virtual_sex_with_virgin_teen_vr_"
-@video.url = "http://www.xvideos.com/video24801799/virtual_sex_with_virgin_teen_vr_"
-@video.image = "/xvideos/4cc5cb9f48dbaae23e034a9dc0401979.23.jpg"
+@video.title = "Hot Redhead Fucks Her Boss! (VR)"
+@video.url = "http://www.xvideos.com/video24882351/THUMBNUM/hot_redhead_fucks_her_boss_vr_"
+@video.image = "/xvideos/db73f7bd6a6f27dd0f17b80c80be3c3d.15.jpg"
 @video.site_name = "XVIDEOS.COM"
-@video.Date = "20161120"
+@video.Date = "20161121"
 @video.save
 
 @video = Video.new
-@video.title = "Cumming Full Circle_ badoinkvr"
-@video.url = "http://jp.pornhub.com/view_video.php?viewkey=ph5830ebe23777d"
-@video.image = "/pornhub/96683101original(m=ecuK8daaaa)12.jpg"
-@video.site_name = "Pornhub"
-@video.Date = "20161120"
+@video.title = "桜木優希音　おもてなしオナニー"
+@video.url = "http://www.afesta.tv/vr/face.php?fid=6d90ed7bef3479c1ac4911256ca411c0"
+@video.image = "/adultfesta/exs041.jpg"
+@video.site_name = "Adult festa VR"
+@video.Date = "20161121"
 @video.save
 
 @video = Video.new
-@video.title = "Everything For You (powered by SexLikeReal)"
-@video.url = "http://jp.pornhub.com/view_video.php?viewkey=ph583094bb736ec"
-@video.image = "/pornhub/96655901original(m=ecuK8daaaa)3.jpg"
+@video.title = "桜木優希音　お風呂場フェラ"
+@video.url = "http://www.afesta.tv/vr/face.php?fid=3ce1f0e1eddecbd4089a566ce1b34c77"
+@video.image = "/adultfesta/exs039-040.jpg"
+@video.site_name = "Adult festa VR"
+@video.Date = "20161121"
+@video.save
+
+@video = Video.new
+@video.title = "桜木優希音　中出しSEX"
+@video.url = "http://www.afesta.tv/vr/face.php?fid=06a9a70509b2a71fbe9da263126b288f"
+@video.image = "/adultfesta/exs042-043.jpg"
+@video.site_name = "Adult festa VR"
+@video.Date = "20161121"
+@video.save
+
+@video = Video.new
+@video.title = "波多野結衣　中出しSEX"
+@video.url = "http://www.afesta.tv/vr/face.php?fid=7f021b56bc8182c3a6762dbe7eae6d16"
+@video.image = "/adultfesta/exs016.jpg"
+@video.site_name = "Adult festa VR"
+@video.Date = "20161121"
+@video.save
+
+@video = Video.new
+@video.title = "波多野結衣　おもちゃ責め"
+@video.url = "http://www.afesta.tv/vr/face.php?fid=7c2249cd0b6abb03b9478de907f964e6"
+@video.image = "/adultfesta/exs015.jpg"
+@video.site_name = "Adult festa VR"
+@video.Date = "20161121"
+@video.save
+
+@video = Video.new
+@video.title = "波多野結衣　お風呂でフェラ"
+@video.url = "http://www.afesta.tv/vr/face.php?fid=245c3887508560c7f270225c8fac3b62"
+@video.image = "/adultfesta/exs013-014.jpg"
+@video.site_name = "Adult festa VR"
+@video.Date = "20161121"
+@video.save
+
+@video = Video.new
+@video.title = "波多野結衣　おもてなしオナニー"
+@video.url = "http://www.afesta.tv/vr/face.php?fid=03444fedda127f7be4a07ad6432ed91d"
+@video.image = "/adultfesta/exs011-012.jpg"
+@video.site_name = "Adult festa VR"
+@video.Date = "20161121"
+@video.save
+
+@video = Video.new
+@video.title = "河音くるみ"
+@video.url = "http://www.afesta.tv/vr/face.php?fid=91b2ab4210d1fa34e192d0a73d245363"
+@video.image = "/adultfesta/exs020-021.jpg"
+@video.site_name = "Adult festa VR"
+@video.Date = "20161121"
+@video.save
+
+@video = Video.new
+@video.title = "河音くるみ　おもてなしオナニー"
+@video.url = "http://www.afesta.tv/vr/face.php?fid=ba4dbe2847e8d965ca7ac34152a1b542"
+@video.image = "/adultfesta/exs019.jpg"
+@video.site_name = "Adult festa VR"
+@video.Date = "20161121"
+@video.save
+
+@video = Video.new
+@video.title = "阿部乃みく・西尾れむ いやらし過ぎる舌使いの濃厚悶絶Wフェラ"
+@video.url = "http://www.afesta.tv/vr/face.php?fid=ea49f10bb404a8197811734ed73b2051"
+@video.image = "/adultfesta/ofks048-1.jpg"
+@video.site_name = "Adult festa VR"
+@video.Date = "20161121"
+@video.save
+
+@video = Video.new
+@video.title = "西条沙羅・篠宮ゆり いやらし過ぎる舌使いの濃厚悶絶Wフェラ"
+@video.url = "http://www.afesta.tv/vr/face.php?fid=53b533f139d668fb3cdd7014a7c8860e"
+@video.image = "/adultfesta/ofks050-1.jpg"
+@video.site_name = "Adult festa VR"
+@video.Date = "20161121"
+@video.save
+
+@video = Video.new
+@video.title = "VR BANGERS-Kirschley Swoon kitchen fuck and blowjob"
+@video.url = "http://jp.pornhub.com/view_video.php?viewkey=ph58316d4d05de4"
+@video.image = "/pornhub/96720901original(m=ecuK8daaaa)12.jpg"
 @video.site_name = "Pornhub"
-@video.Date = "20161120"
+@video.Date = "20161121"
 @video.save
 
